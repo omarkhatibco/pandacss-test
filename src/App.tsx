0c2f406fe53eb8d775519980ef10a02f85c85f6f
Omar Khatib <omar@omarkhatib.co>
2023-06-26 03:08:39 +0200

    lineClamp

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { AspectRatio, Divider } from '../styled-system/jsx';
-import { button } from '../styled-system/recipes';
+import { AspectRatio, Divider, styled } from '../styled-system/jsx';
 
 function App() {
   return (
@@ -9,17 +8,21 @@ function App() {
       </AspectRatio>
       {/* <Box as='article'>test Panda css</Box> */}
       <Divider color={'tonal.secondary.10'} />
-
-      <button className={button()}>test</button>
-      <button
-        className={button({
-          visual: 'edgy',
-          size: 'lg',
-          shape: 'square',
-        })}
-      >
-        test
-      </button>
+      <styled.p lineClamp={1}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at
+        posuere enim. In hendrerit arcu eu leo molestie faucibus. Aliquam erat
+        volutpat. Curabitur dignissim at justo ac vehicula. Vivamus mollis, nunc
+        nec commodo volutpat, tortor mi mattis ipsum, id consequat neque est
+        eget augue. Sed facilisis volutpat magna ac dictum. Sed quis pretium
+        orci. Cras vestibulum vitae dolor in tempus. Sed nec laoreet ante.
+        Pellentesque aliquet quis libero a posuere. Pellentesque convallis metus
+        nec enim aliquam, in sagittis velit pretium. Nam dignissim placerat
+        metus, nec dictum mi convallis vel. Sed vehicula dolor velit, eu
+        dignissim massa rutrum at. Suspendisse maximus est sit amet arcu
+        pharetra, at egestas tortor lacinia. Etiam sagittis, leo a vestibulum
+        egestas, arcu turpis volutpat nibh, sit amet feugiat dolor neque vel
+        ipsum. Cras in libero nibh.
+      </styled.p>
     </>
   );
 }
