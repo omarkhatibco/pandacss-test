fcd10474f03592a6517f84f70120a97f156af473
Omar Khatib <omar@omarkhatib.co>
2023-07-04 00:42:36 +0200

    update to latest version

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { AspectRatio, Divider } from '../styled-system/jsx';
-
-import { Button } from './button/Button';
+import { AspectRatio } from '../styled-system/jsx';
 
 function App() {
   return (
     <>
-      <AspectRatio ratio={1 / 2}>
+      <AspectRatio ratio={16 / 9}>
         <img loading='lazy' src='https://picsum.photos/seed/tech/600/400' />
       </AspectRatio>
-      {/* <Box as='article'>test Panda css</Box> */}
-      <Divider color={'tonal.secondary.10'} />
-      <Button>Test</Button>
     </>
   );
 }
